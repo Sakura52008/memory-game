18ab0c8a23ec580c6fc2b58ac96f8bffde4afbf1
Sakura52008 <menzanorken@gmail.com>
2025-02-05 11:01:56 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MemoryGame from './MemoryGame'; // Импорт компонента MemoryGame
+
+function App() {
+  return (
+    <div className="App">
+      <MemoryGame />
+    </div>
+  );
+}
+
+export default App;
